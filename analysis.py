822736f78aa2ab0ef8cd4da1449171daba0c2797
DasBuchta <baloghmatus01@gmail.com>
2022-06-06 22:48:28 +0200

    feat: Some basic analysis

---
+++ analysis.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def connect_db():
+    return sqlite3.connect('powerplants.sqlite3')
+
+
+if __name__ == "__main__":
+    db = connect_db()
+    cursor = db.cursor()
+
+    # cursor.execute("""SELECT country, COUNT() FROM powerplants GROUP BY country""")
+    cursor.execute("""SELECT name, primary_fuel FROM powerplants WHERE country='SVK'""")
+    cursor.execute("""SELECT primary_fuel, SUM(generation_gwh_2013), SUM(generation_gwh_2019) 
+                        FROM powerplants GROUP BY primary_fuel""")
+    cursor.execute("""SELECT primary_fuel, SUM(generation_gwh_2013), SUM(generation_gwh_2019) 
+                        FROM powerplants GROUP BY primary_fuel""")
+
+    for row in cursor:
+        print(row)
+
+    db.close()
